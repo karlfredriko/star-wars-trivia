ef27e2bd1bffff32f277f9cfc294c4bc69cef59e
karlfredriko <kf.holmsten@gmail.com>
2023-03-08 15:34:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("hi");
+
+//DOM REFERENCES
+let containerOne = document.querySelector("#containerOne")
+let containerTwo = document.querySelector("#containerTwo")
+let charOne = document.querySelector("#charSelectOne")
+
+let fetchData = async (peopleVal) => {
+    let data = await fetch(`https://swapi.dev/api/people/${peopleVal}/`);
+    let json = data.json();
+    return json;
+};
+
+let printData = async () => {
+    let data = await fetchData(charOne.value);
+    console.log(data)
+    containerOne.innerHTML += `<p>${data.name}</p>`;
+    console.log(data);
+};
+
+charOne.addEventListener("change", () => {
+    console.log("charOne reporting")
+    printData()
+})
+
